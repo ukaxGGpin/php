528be6aaa221a310b4dec896e8f4855150f2ad06
sio1 <sio1@c106-09.220056s.local>
2023-11-10 17:08:20 +0100

    commited

--- traitement.php
+++ traitement.php
@@ -1,13 +1,15 @@
-
 <!DOCTYPE html>
 <html lang="fr">
+  
 <head>
   <title>formulaire 1</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" type="text/css" href="styles.css" />
 </head>
+
  <?php
+
 echo "<table>";
 
 for($x = 0; $x <=10 ; $x++) {
