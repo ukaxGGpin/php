e603bae7c7fd1c9e9d7c0e3403b359ad3b2bcb45
sio1 <sio1@c106-09.220056s.local>
2023-11-10 14:39:06 +0100

    first commit

---
+++ traitement.php
@@ -0,0 +1,17 @@
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <title>formulaire 1</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+ <?php
+
+echo "Hello World !!";
+
+$nom=$_REQUEST["nom"];
+echo $nom. " est ";
+?>
+<body>
